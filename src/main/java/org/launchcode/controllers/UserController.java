331fa321c56cb32f8fcad655d8d5594ba3f2298c
Irina Sharapova <ir.ur.smirnova@gmail.com>
2024-10-15 00:21:34 -0400

    Studio Spa day added part 1. Rendering the Add User Form

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,21 @@
+package org.launchcode.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("add")
+    public String displayAddUserForm() {
+        return "user/add";
+    }
+
+}
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+
+    }
+    }
+
