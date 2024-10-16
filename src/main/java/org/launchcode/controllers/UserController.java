28ce8f36bc594b9c0e4af1093a340c3f1a1f36b7
Irina Sharapova <ir.ur.smirnova@gmail.com>
2024-10-16 14:26:17 -0400

    Studio Spa day added.

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,8 +1,10 @@
 package org.launchcode.controllers;
 
+import org.springframework.ui.Model;
+
+import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("user")
@@ -13,9 +15,20 @@ public class UserController {
         return "user/add";
     }
 
-}
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
 
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verifyPassword) {
+
+        if (!user.getPassword().equals(verifyPassword)) {
+            model.addAttribute("username" , user.getUsername());
+            model.addAttribute("email" , user.getEmail());
+            model.addAttribute("error", "PASSWORD DO NOT MATCH!");
+            return "user/add";
+        }
+        model.addAttribute("user", user);
+        return "user/index";
     }
-    }
+
+}
+
 
