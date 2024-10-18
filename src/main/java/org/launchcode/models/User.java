91a941b11f209ab9105b7f550934cdf9425fa7c5
Irina Sharapova <ir.ur.smirnova@gmail.com>
2024-10-17 22:55:02 -0400

    Studio Spa day  User Validation added

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,8 +1,21 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters")
     private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
 
     public User() {
